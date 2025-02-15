7216e6d803f4e4e46bd22976fecda819832bbfba
Sridevi2108 <sridevi21082003@gmail.com>
2025-02-15 22:35:50 +0530

    Add files via upload

---
+++ 54_optimized.py
@@ -0,0 +1,22 @@
+class Solution:
+    def spiralOrder(self, matrix):
+        result = []
+        while matrix:
+            result += matrix.pop(0)
+            if matrix and matrix[0]:
+                for row in matrix:
+                    result.append(row.pop())
+            if matrix:
+                result += matrix.pop()[::-1]
+            if matrix and matrix[0]:
+                for row in matrix[::-1]:
+                    result.append(row.pop(0))
+        return result
+if __name__=="__main__":
+    matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    solution=Solution()
+    result=solution.spiralOrder(matrix)
+    print(result)
+
+
+
